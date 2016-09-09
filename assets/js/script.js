5db7980b2cea532a007150e7260b41e0b1f2d097
Ryan French <ryan.lloyd.french@gmail.com>
2016-09-09 19:34:57 +1200

    Setup animation and smooth state.

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+  'use strict';
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 1000,
+      render: function ($container) {
+        $container.addClass('is-exiting');
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        $container.removeClass('is-exiting');
+        $container.html($newContent);
+      }
+    }
+  },
+  smoothState = $('#animate-wrapper').smoothState(options).data('smoothState');
+});
